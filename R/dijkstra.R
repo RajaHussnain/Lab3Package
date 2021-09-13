57c4a425f1c96194b58efa4df24d56005d4f59f5
RajaHussnain <hussnainkhalid92@gmail.com>
2021-09-13 23:49:45 +0200

    Changes 2.6.5 1: Description error fixed. 2: Dijkstra function structure build. 3: Dijkstra documentation created.

--- R/dijkstra.R
+++ R/dijkstra.R
@@ -4,7 +4,21 @@
 #' @export
 
 dijkstra <- function(graph, init_node){
+  #' @param graph Data frame
+  #' @param init_node Initial node
+  #' @return Return Shortest Path
+  #' @examples
+  #' wiki_graph <- data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
+  #'                         v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
+  #'                         w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
+  #' dijkstra(wiki_graph, 1)
+  #' dijkstra(wiki_graph, 3)
+  #' @source \url{https://en.wikipedia.org/wiki/Dijkstra\%27s_algorithm}
+
+  # Numerical Input check od initial node
   stopifnot(is.numeric(init_node) == TRUE)
+
+  print(init_node)
 
 }
 
